c6b97029b792942a6c90bfa23637199bdabf02ff
devgitops <apaul07@icloud.com>
2021-05-27 02:34:39 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+    var flag = checkdata();
+    
+    if(flag){
+
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+
+        var interest = principal*rate*years/100;
+        var amount = principal + interest;
+        var year = new Date().getFullYear() + parseInt(years);
+
+        var res_msg = 
+        "If you deposit <mark>"+ principal +"</mark>,<br>\
+        at an interest rate of <mark>"+ rate +"%</mark>.<br>\
+        You will receive an amount of <mark>"+ amount +"</mark>,<br>\
+        in the year <mark>"+ year +"</mark><br>";
+
+        document.getElementById("result").innerHTML=res_msg
+
+    }
+    
+
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+function checkdata(){
+    var p = document.getElementById("principal");
+
+    if(p.value<=0){
+        alert('Enter a positive number')
+        p.focus();
+        return false
+        
+    }
+
+    return true
     
 }
-        
